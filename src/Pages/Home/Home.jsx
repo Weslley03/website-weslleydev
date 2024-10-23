a28db1b345b78e0daa9dc0096053b5aaf5ab2a1b
oweslley03 <wesleymoraeswv03@hotmail.com>
2024-10-22 21:48:53 -0300

    foi corrigido a nomeclatura do nome de algumas linguagens, apenas por fins esteticos

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -45,13 +45,13 @@ function Home(){
                         <DivTexto>
                             <h1> Welcome Outsider</h1>
                             <p>My name is Weslley, Im a FullStack web developer, I currently work in an educational systems company and as a freelancer. I have several interesting projects to present to you!</p>
-                            <p>I am proficient in development with NODE.JS, languages such as JAVASCRIPT and TYPESCRIPT, practical knowledge with EXPRESS.JS, NEST.JS, REACT.JS, I have also used ORMs such as PRISMA and MONGOOSE, JWT for authentication and SWAGGER for documentation, experience that provides agile and secure development, I also have skills with MYSQL WORKBENCH for relational structures and MONGODB for non-relational structures.</p>
+                            <p>I am proficient in development with NODEJS, languages such as JAVASCRIPT and TYPESCRIPT, practical knowledge with EXPRESSJS, NESTJS, REACTJS, I have also used ORMs such as PRISMA and MONGOOSE, JWT for authentication and SWAGGER for documentation, experience that provides agile and secure development, I also have skills with MYSQL WORKBENCH for relational structures and MONGODB for non-relational structures.</p>
                         </DivTexto>
                     </DivParteTextual>
 
                     <DivProjetos>
                         <DivOpcoes>
-                            <h2>my Projects</h2>
+                            <h2>my projects</h2>
                             <Link to={'/wfnews'} style={{textDecoration:'none'}}>
                                 <span>WFNews (FULL SITE)</span>
                             </Link>
