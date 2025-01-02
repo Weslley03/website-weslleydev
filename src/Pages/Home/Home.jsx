f9697418885a3f90117bb6a26ef3be0b456e1b41
oweslley <wesleymoraeswv03@hotmail.com>
2025-01-01 23:09:06 -0300

    texto update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -44,10 +44,8 @@ function Home(){
                     <DivParteTextual>
                         <DivTexto>
                             <h1> Welcome Outsider</h1>
-                            <p>My name is Weslley, Im a FullStack web developer, I currently work in an educational systems company and as a freelancer. I have several interesting projects to present to you!</p>
-                            <p>I am proficient in development with <strong style={{color: '#417E38'}}>Node.js</strong>, languages such as <strong style={{color: '#EED81C'}}>
-                                JavaScript</strong> and <strong style={{color: '#3178C5'}}>TypeScript</strong>, practical knowledge with <strong style={{color: '#EA2859'}}>Nest.js</strong>, <strong style={{color: '#3998B6'}}>React.js</strong>, I have also used 
-                            ORMs such as <strong style={{color: '#4C51BF'}}>Prisma</strong> and <strong style={{color: '#A03333'}}>Mongoose</strong>, <strong style={{color: '#FF45CE'}}>JWT</strong> for authentication and SWAGGER for documentation, experience that provides agile and secure development, I also have skills with <strong>MySQL Workbench</strong> for relational structures and <strong>MongoDB</strong> for non-relational structures.</p>
+                            <p>my name is Weslley, i'm a fullstack developer and i have a few years of experience in the market. i currently work in an EdTech and as a freelancer, and this way, i developed technical and non-technical skills, such as teamwork and excellent problem solving.</p>
+                            <p>i'm proficient in Node.js development, building applications with <strong style={{color: '#EED81C'}}>JavaScript</strong>, taking care of issues such as scalability, performance, code readability and proper use of tools. I really like using <strong style={{color: '#3178C5'}}>TypeScript</strong> for the backend side, i belive it brings confidence to the code and i can use all my Javascript knowledge. I prioritize testing in my Applications and i like to use libs and frameworks like <strong style={{color: '#EA2859'}}>Nest.js</strong> and <strong style={{color: '#3998B6'}}>React.js</strong> to make my live as a developer easier.</p>
                         </DivTexto>
                     </DivParteTextual>
 
@@ -55,19 +53,16 @@ function Home(){
                         <DivOpcoes>
                             <h2>my projects</h2>
                             <Link to={'/wfnews'} style={{textDecoration:'none'}}>
-                                <span>WFNews (FULL SITE)</span>
-                            </Link>
-                            <Link to={'/websockets'} style={{textDecoration:'none'}}>
-                                <span>ONLINE CHAT with WebSockets</span>
-                            </Link>
-                            <Link to={'/video-streaming'} style={{textDecoration:'none'}}>
-                                <span>Video-Streaming-NODEJS</span>                           
-                            </Link>
-                            <Link to={'/crudfullstack'} style={{textDecoration:'none'}}>
-                                <span>CRUD-FullStack-Node-React-MySQL</span>                       
+                                <span>break news website</span>
                             </Link>
                             <Link to={'/lhotse-learning-english'} style={{textDecoration:'none'}}>
-                                <span>LHOTSE-Learning-English</span>                       
+                                <span>lhotse-learning english</span>                       
+                            </Link>
+                            <Link to={'https://github.com/Weslley03/mini-vue-framework'} style={{textDecoration:'none'}}>
+                                <span>mini-vue-framework</span>
+                            </Link>
+                            <Link to={'https://github.com/Weslley03/cache-decorator-with-redis'} style={{textDecoration:'none'}}>
+                                <span>cache-decorator-with-redis</span>                           
                             </Link>
                         </DivOpcoes>
                     </DivProjetos>
